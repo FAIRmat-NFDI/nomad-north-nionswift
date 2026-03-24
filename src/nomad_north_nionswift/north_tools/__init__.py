8fb18699a06c3b7bcc0dcabe97c80c1986119332
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-03-24 11:00:59 +0100

    correct init

--- src/nomad_north_nionswift/north_tools/__init__.py
+++ src/nomad_north_nionswift/north_tools/__init__.py
@@ -2,14 +2,18 @@ from nomad.config.models.north import NORTHTool
 from nomad.config.models.plugins import NorthToolEntryPoint
 
 nionswift = NORTHTool(
-    short_description='Jupyter Notebook server in NOMAD NORTH for NOMAD plugin nomad-north-nionswift.',
+    short_description='Use Bruker nionswift to visualize and analyze electron microscopy data in NOMAD.',
     image='ghcr.io/fairmat-nfdi/nomad-north-nionswift:main',
-    description='Jupyter Notebook server in NOMAD NORTH for NOMAD plugin nomad-north-nionswift.',
+    description="""### **nionswift**:
+
+    [Software for electron microscopy data analysis and visualization](https://nionswift.readthedocs.io/en/stable/)
+
+    [Conference proceedings about the software](https://doi.org/10.1017/S1431927614007272)""",
     external_mounts=[],
-    file_extensions=['ipynb'],
+    file_extensions=['tiff, tif, dm3, dm4, hdf5, h5, nsproj'],
     icon='https://raw.githubusercontent.com/FAIRmat-NFDI/nomad-north-nionswift/main/src/nomad_north_nionswift/north_tools/nionswift/nionswift.png',
     image_pull_policy='Always',
-    default_url='/lab',
+    default_url='/desktop',
     maintainer=[{'email': 'markus.kuehbach@physik.hu-berlin.de', 'name': 'Markus Kühbach'}],
     mount_path='/home/jovyan',
     path_prefix='lab/tree',
