8cd1910dbf3ff3c73c26b3855177ba0c22f30674
Markus Kühbach <mkuehbach@users.noreply.github.com>
2026-03-27 14:27:23 +0100

    Fixes lauri (#8)
    * fixes suggested by lauri
    
    * test
    
    ---------
    
    Co-authored-by: mkuehbach <markus.kuehbach@physik.hu-berlin.de>

--- tests/north_tools/test_north_tools.py
+++ tests/north_tools/test_north_tools.py
@@ -1,9 +1,7 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails
-    from nomad_north_nionswift.north_tools import north_entry_point
+    from nomad_north_nionswift.north_tools import nionswift
 
-    expected_id = 'nomad-north-nionswift-nionswift'
     assert (
-        north_entry_point.id_url_safe == expected_id
-        or north_entry_point.id == 'nomad-north-nomad-north-nionswift'
+        nionswift.id_url_safe == 'nionswift' or nionswift.id == 'nomad-north-nionswift'
     ), 'NORTHTool entry point has incorrect id or id_url_safe'
